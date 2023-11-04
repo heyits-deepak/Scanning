12ee2c923e86d20022679ce3fd3c26a76a5d2cf6
Deepak <dsynr2003@gmail.com>
2023-11-04 12:58:32 +0530

    This is our first commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
 import re
-import ssl
-import urllib.request
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Allow requests from your React frontend
+# Allow requests from your React frontend3
 origins = ["http://localhost:5173"]
 
 app.add_middleware(
@@ -24,10 +22,32 @@ app.add_middleware(
 # Define a function to check for common vulnerabilities and security aspects
 def assess_vulnerabilities(url):
     try:
+        # Check if the URL starts with "https://" or "http://"
+        if not (url.startswith("https://") or url.startswith("http://")):
+            # Prepend "https://" to the URL and send a GET request
+            try:
+                response = requests.get("https://" + url)
+                url = "https://" + url
+            except requests.exceptions.RequestException:
+                # If the request raises an exception, prepend "http://" to the URL and send another GET request
+                try:
+                    response = requests.get("http://" + url)
+                    url = "http://" + url
+                except requests.exceptions.RequestException:
+                    # If both requests raise exceptions, raise an HTTPException
+                    raise HTTPException(
+                        status_code=500,
+                        detail="Error: Unable to connect to the website",
+                    )
+
         # Send a GET request to the provided URL
         response = requests.get(url)
         response.raise_for_status()
 
+        # Get the final URL after redirects
+        final_url = response.url
+        print(response)
+        print(response.url)
         # Parse the HTML content
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -35,9 +55,7 @@ def assess_vulnerabilities(url):
         vulnerabilities = []
 
         # Check for common vulnerabilities using regular expressions
-        common_vulnerabilities = {
-            "XSS": [r"<script\b", r"alert\(", r"onerror="],
-            "SQL Injection": [r"\bSELECT\b", r"\bDROP\b", r"\bUNION\b"],
+        common_vulnerabilities = { "XSS": [r"<script\b", r"alert\(", r"onerror="],
             "Command Injection": [
                 r";\s*ls",
                 r"&&\s*rm\s*-rf",
@@ -45,31 +63,62 @@ def assess_vulnerabilities(url):
             ],
             "Insecure Password Storage": [r'type=["\']?password["\']?'],
             "Cross-Site Request Forgery (CSRF)": [r"csrf[-_]token", r"csrf_token"],
-            "Insecure Direct Object References (IDOR)": [
-                r"\buser_id\b=1",
-                r"\bfile\b=../../etc/passwd",
+            "Insecure Direct Object References (IDOR)": [ r"\buser_id\b=1", r"\bfile\b=../../etc/passwd",
             ],
             "Sensitive Data Exposure": [r"\bpassword\b", r"\bapikey\b", r"\bsecret\b"],
             "Security Misconfiguration": [r"404\s*Not Found", r"403\s*Forbidden"],
             "Broken Authentication": [r"\blogin\b", r"authentication\s*failed"],
             "Insecure Deserialization": [r"phpserialize", r"pickle\.load\("],
+            "Security Misconfiguration": [
+            r"404\s*Not Found",
+            r"403\s*Forbidden",
+            r"500\s*Internal Server Error",
+            r"server\s*error",
+            r"misconfigured",
+            r"access\s*denied",
+            r"directory\s*listing\s*is\s*disabled",
+            r"stack\s*trace",
+            r"traceback",
+            ],
+            "Secure Design": [
+            r"\bleast\s*privilege\b",
+            r"\bprinciple\s*of\s*least\s*privilege\b",
+            r"\bseparation\s*of\s*duties\b",
+            r"\bdefense\s*in\s*depth\b",
+            r"\bsecurity\s*by\s*design\b",
+            r"\bsecure\s*architecture\b",
+            r"\bauthentication\s*and\s*authorization\b",
+            r"\bsecure\s*communication\b",
+            r"\bsecure\s*file\s*uploads\b",
+            r"\binput\s*validation\b",
+            r"\boutput\s*encoding\b",
+            r"\berror\s*handling\b",
+            r"\bsecurity\s*policies\b",
+            r"\bdata\s*classification\b",
+            ],
+            "Missing Rate Limiting": [r"429\s*Too Many Requests"],
+            "Missing HTTP Security Headers": [
+                r"X-Frame-Options",
+                r"X-XSS-Protection",
+                r"X-Content-Type-Options",
+                r"Content-Security-Policy",
+            ],
+
         }
 
         for vulnerability, patterns in common_vulnerabilities.items():
             for pattern in patterns:
                 if re.search(pattern, soup.text, re.IGNORECASE):
                     vulnerabilities.append(vulnerability)
-
-        # Check if the website uses HTTPS
-        if not url.startswith("https://"):
+# Check if the website uses HTTPS
+        if not final_url.startswith("https://"):
             vulnerabilities.append("Not Using HTTPS")
 
         # Check for SSL certificate
         try:
-            context = ssl.create_default_context()
-            with urllib.request.urlopen(url, context=context) as response:
-                pass
-        except (ssl.SSLError, urllib.error.URLError):
+            # This will raise an SSLError if the certificate is not valid
+            requests.get(url, verify=True)
+        except requests.exceptions.SSLError:
             vulnerabilities.append("SSL Certificate Issue")
 
         if vulnerabilities:
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -16,47 +16,57 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await axios.get('http://localhost:8000/vulnerability', {
+      const response = await axios.get("http://localhost:8000/vulnerability", {
         params: { url },
       });
       setResult(response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="appStyle">
-      <h1>URL Security Assessment</h1>
-      <form className="formStyle" onSubmit={handleSubmit}>
-        <input
-          className="inputStyle"
-          type="text"
-          placeholder="Enter URL"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <button className="buttonStyle" type="submit" disabled={loading}>
-          Assess
-        </button>
-      </form>
-      {loading ? (
-        <div className="loadingStyle">
-          <div className="spinnerStyle"></div>
-        </div>
-      ) : null}
-      {result && (
-        <div className="resultStyle">
-          <h2>Assessment Result for {url}</h2>
-          <ul>
-            {result.assessment.vulnerabilities.map((vulnerability, index) => (
-              <li key={index}>{vulnerability}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className="body">
+      <div className="appStyle">
+        <h1>URL Security Assessment</h1>
+        <hr />
+        <form className="formStyle" onSubmit={handleSubmit}>
+          <div className="inp">
+            <input
+              className="inputStyle"
+              type="text"
+              placeholder="Enter URL ....."
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            <button className="buttonStyle" type="submit" disabled={loading}>
+              {/* <i class="fa-solid fa-magnifying-glass"></i> */}
+              <img
+                src="../images/search-removebg-preview.png"
+                alt=""
+                srcset=""
+              />
+            </button>
+          </div>
+        </form>
+        {loading ? (
+          <div className="loadingStyle">
+            <div className="spinnerStyle"></div>
+          </div>
+        ) : null}
+        {result && (
+          <div className="resultStyle">
+            <h2>Assessment Result for {url}</h2>
+            <ul>
+              {result.assessment.vulnerabilities.map((vulnerability, index) => (
+                <li key={index}>{vulnerability}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
