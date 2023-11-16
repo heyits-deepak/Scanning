fdfcddd471b6a0f131641e67eaeda9c6ef194c84
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2023-11-16 22:55:27 +0530

    new page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import VulSolution  from "./VulSolution";
 
 function App() {
   const [url, setUrl] = useState("");
@@ -32,7 +33,7 @@ function App() {
       <div className="appStyle">
         <h1>
           <i className="fa-solid fa-skull fa-beat-fade"></i>
-          <span className="blink">URL Security Assessment    </span>
+          <span className="blink">URL Security Assessment </span>
           <i className="fa-solid fa-skull fa-beat-fade"></i>
         </h1>
 
@@ -67,19 +68,23 @@ function App() {
              */}
             </div>
             <div className="bottom-result">
-             <div className="vul">
-              <ul>
-                {result.assessment.vulnerabilities.map(
-                  (vulnerability, index) => (
-                    <li key={index}>{vulnerability}</li>
-                  )
-                )}
-              </ul>
-            </div>
-           
+              <div className="vul">
+                <ul>
+                  {result.assessment.vulnerabilities.map(
+                    (vulnerability, index) => (
+                      <li key={index}>{vulnerability}</li>
+                    )
+                  )}
+                </ul>
+              </div>
             </div>
             <div className="time">
-              <img className="time-sand" src="../images/output-onlinegiftools.gif" alt="" srcset="" / >
+              <img
+                className="time-sand"
+                src="../images/output-onlinegiftools.gif"
+                alt=""
+                srcset=""
+              />
               <p>
                 <span> 2</span> Sec
               </p>
@@ -87,6 +92,21 @@ function App() {
             {/* <div className="link">
               <p>Click here to see </p>
             </div> */}
+
+             {/* last div logic to hide or unhide the last div */}
+            {result.assessment.vulnerabilities.includes(
+              "No Common Vulnerabilities Detected"
+            ) ? null : (
+              <div className="last">
+                <img
+                  className="hand"
+                  src="../images/giphy.gif"
+                  alt=""
+                  srcset=""
+                />
+                <a href="../src/page.html"><button className="click" >CLICK HERE</button></a>
+              </div>
+            )}
           </div>
         )}
       </div>
