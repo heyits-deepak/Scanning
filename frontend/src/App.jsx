62e0b093fc524bd6f948824cc18ad1075af1b4f2
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2023-11-17 11:33:25 +0530

    new page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -104,7 +104,7 @@ function App() {
                   alt=""
                   srcset=""
                 />
-                <a href="../src/page.html"></a>
+                 <a href="../src/page.html"><button className="click" >CLICK HERE</button></a>
               </div>
             )}
           </div>
